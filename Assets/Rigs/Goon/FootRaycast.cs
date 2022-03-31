172ff57c77c982074ee89c4c5022aa68e9373604
Nick Pattison <pattison.nick@gmail.com>
2022-03-31 11:01:13 -0400

    Week 10 lab, part 2

--- Assets/Rigs/Goon/FootRaycast.cs
+++ Assets/Rigs/Goon/FootRaycast.cs
@@ -8,34 +8,44 @@ public class FootRaycast : MonoBehaviour {
     private float distanceBetweenGroundAndIK = 0;
 
     private Quaternion startingRot;
+    ///<summary>The world-space position of the ground above/below the foot IK.</summary>
+    private Vector3 groundPosition;
+    ///<summary>The world-space rotation for the foot to be aligned w/ ground.</summary>
+    private Quaternion groundRotation;
 
     void Start() {
+
         startingRot = transform.localRotation;
         distanceBetweenGroundAndIK = transform.localPosition.y;
     }
 
     void Update() {
-        
 
-        Vector3 origin = transform.position + Vector3.up * raycastLength/2;
+        //FindGround();
+
+    }
+
+    private void FindGround() {
+
+        Vector3 origin = transform.position + Vector3.up * raycastLength / 2;
         Vector3 direction = Vector3.down;
 
         // draw the ray in the scene:
         Debug.DrawRay(origin, direction * raycastLength, Color.blue);
 
         // check for collision with ray:
-        if(Physics.Raycast(origin, direction, out RaycastHit hitInfo, raycastLength)){
+        if (Physics.Raycast(origin, direction, out RaycastHit hitInfo, raycastLength))
+        {
 
             // finds ground position
-            transform.position = hitInfo.point + Vector3.up * distanceBetweenGroundAndIK;
+            groundPosition = hitInfo.point + Vector3.up * distanceBetweenGroundAndIK;
 
             // convert starting rotation into world-space
             Quaternion worldNeutral = transform.parent.rotation * startingRot;
 
             // finds ground rotation
-            transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal) * worldNeutral;
+            groundRotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal) * worldNeutral;
 
         }
-
     }
 }
--- Assets/Rigs/Goon/GoonMovement.cs
+++ Assets/Rigs/Goon/GoonMovement.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class GoonMovement : MonoBehaviour {
     
+
+    public FootRaycast footLeft;
+    public FootRaycast footRight;
+
     public float speed = 2;
+    public float walkSpreadX = .2f;
+    public float walkSpreadY = .4f;
+    public float walkSpreadZ = .8f;
+    public float walkFootSpeed = 4;
     private CharacterController pawn;
 
     void Start() {
         pawn = GetComponent<CharacterController>();
     }
 
-    
     void Update() {
         float v = Input.GetAxis("Vertical");
         float h = Input.GetAxis("Horizontal");
@@ -22,5 +29,30 @@ public class GoonMovement : MonoBehaviour {
 
         pawn.SimpleMove(move * speed);
 
+        AnimateWalk();
     }
+
+    delegate void MoveFoot(float time, float x, FootRaycast foot);
+    void AnimateWalk(){
+
+        MoveFoot moveFoot = (t, x, foot) => {
+            
+            float y = Mathf.Cos(t) * walkSpreadY;
+            float z = Mathf.Sin(t) * walkSpreadZ;
+
+            if(y < 0) y = 0;
+            y += .177f;
+
+            foot.transform.localPosition = new Vector3(x, y, z);
+        };
+
+        float t = Time.time * walkFootSpeed;
+
+        moveFoot.Invoke(t, -walkSpreadX, footLeft);
+        moveFoot.Invoke(t + Mathf.PI, walkSpreadX, footRight);
+
+
+    }
+
+
 }
